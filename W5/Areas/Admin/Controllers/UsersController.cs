2dc2589113b6677624448cc5a284968285a0f737
Marcin <jk7724@protonmail.com>
2021-04-12 14:17:36 +0200

    Add authorization

---
+++ W5/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utilities;
+
+namespace W5.Areas.Admin.Controllers
+{
+    [Authorize(Roles = SD.Role_Admin)]
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
